f54445426789596ff402b566d63575a2e448581b
bennjoy <ben@njoystudentliving.co.uk>
2025-10-14 22:31:40 +0100

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/', // Use this for custom domains
+  build: {
+    outDir: 'dist'
+  }
 })
